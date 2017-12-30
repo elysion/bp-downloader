44fc1c2dc359d18d049fcc12e4f5ceec2db9612a
Miko Kiiski <miko.kiiski@reaktor.fi>
2017-12-30 19:55:03 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const bpApi = require('bp-api')
+const fs = require('fs')
+
+const args = require('optimist')
+  .usage('Login and download available music from Beatport.\n\nUsage: $0')
+  .demand(['c', 'd'])
+  .alias('d', 'downloads-dir')
+  .alias('c', 'credentials-file')
+  .argv
+
+const downloadsDir = args['downloads-dir'];
+const { username, password } = require(args['credentials-file']);
+
+if (!fs.existsSync(downloadsDir)) {
+  fs.mkdirSync(downloadsDir);
+}
+
+console.log(`Logging in as ${username}`)
+
+bpApi.initAsync(username, password)
+  .tap(() => console.log('Login successful'))
+  .tap(() => console.log('Getting available downloads'))
+  .then(session =>
+    session.getAvailableDownloadIdsAsync()
+      .tap(ids => console.log(`Found ${ids.length} available tracks`))
+      .mapSeries(id => session.downloadTrackWithIdAsync(id)) // this does not work point free!
+      .map(request =>
+        request.on('response',
+          res => {
+            const disposition = res.headers['content-disposition']
+            const startString = 'filename=\"'
+            const endString = '\"'
+            const start = disposition.indexOf(startString) + startString.length
+            const end = disposition.indexOf(endString, start)
+            const filename = disposition.substring(start, end)
+            console.log('Downloading:', filename)
+
+            return res.pipe(fs.createWriteStream(`${downloadsDir}/${filename}`))
+          })))
