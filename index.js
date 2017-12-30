21dbdbd9d1d12c7e2a96028f699baa491b7b5661
Miko Kiiski <miko.kiiski@reaktor.fi>
2017-12-30 21:40:54 +0200

    Implement support for ignore file

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const bpApi = require('bp-api')
 const fs = require('fs')
+const R = require('ramda')
 
 const args = require('optimist')
   .usage('Login and download available music from Beatport.\n\nUsage: $0')
   .demand(['c', 'd'])
   .alias('d', 'downloads-dir')
+  .describe('d', 'Target directory where the script will download the tracks to')
   .alias('c', 'credentials-file')
+  .describe('c', `JSON file containing credentials used to log into Beatport \
+(format: {"username": "YOUR_BEATPORT_USERNAME", "password": "YOUR_BEATPORT_PASSWORD"})`)
+  .alias('i', 'ignore-file')
+  .describe('i', `File to log downloaded track ids into. \
+This also works as an input to prevent downloading already downloaded tracks`)
   .argv
 
 const downloadsDir = args['downloads-dir'];
 const { username, password } = require(args['credentials-file']);
+const ignoreFile = args['ignore-file']
+
+let ignored = []
+if (ignoreFile) {
+  if (!fs.existsSync(ignoreFile)) {
+    fs.closeSync(fs.openSync(ignoreFile, 'w'));
+  } else {
+    ignored = fs.readFileSync(ignoreFile, {encoding: 'utf8'}).split('\n').map(Number)
+  }
+}
 
 if (!fs.existsSync(downloadsDir)) {
   fs.mkdirSync(downloadsDir);
@@ -24,17 +41,28 @@ bpApi.initAsync(username, password)
   .then(session =>
     session.getAvailableDownloadIdsAsync()
       .tap(ids => console.log(`Found ${ids.length} available track(s)`))
-      .mapSeries(id => session.downloadTrackWithIdAsync(id)) // this does not work point free!
-      .map(request =>
-        request.on('response',
-          res => {
-            const disposition = res.headers['content-disposition']
-            const startString = 'filename=\"'
-            const endString = '\"'
-            const start = disposition.indexOf(startString) + startString.length
-            const end = disposition.indexOf(endString, start)
-            const filename = disposition.substring(start, end)
-            console.log('Downloading:', filename)
+      .then(R.without(ignored))
+      .tap(ids => console.log(`Found ${ids.length} new track(s)`))
+      .mapSeries(id => session.downloadTrackWithIdAsync(id)
+        .tap(request =>
+          request.on('response',
+            res => {
+              const disposition = res.headers['content-disposition']
+              const startString = 'filename=\"'
+              const endString = '\"'
+              const start = disposition.indexOf(startString) + startString.length
+              const end = disposition.indexOf(endString, start)
+              const filename = disposition.substring(start, end)
+              console.log('Downloading:', filename)
 
-            return res.pipe(fs.createWriteStream(`${downloadsDir}/${filename}`))
-          })))
+              return res
+                .pipe(fs.createWriteStream(`${downloadsDir}/${filename}`))
+                .on('finish', () => {
+                  console.log(`Done downloading ${filename}`)
+                  if (ignoreFile) {
+                    console.log(`Adding ${id} to ignore file`)
+                    fs.appendFileSync(ignoreFile, `${id}\n`)
+                  }
+                })
+              })
+            )))
